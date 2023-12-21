e3df604ca7ae1ed6f4a7c96cc361895e53c33295
Arti <65903469+artigupta13@users.noreply.github.com>
2023-12-21 13:17:15 -0700

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
+const authenticatedUser = require('./router/auth_users.js').authenticatedUser;
 const app = express();
 
 app.use(express.json());
@@ -11,7 +11,24 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,data)=>{
+            console.log("user",data);
+            if(!err){
+                req.user = data.user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }  
 });
  
 const PORT =5000;
